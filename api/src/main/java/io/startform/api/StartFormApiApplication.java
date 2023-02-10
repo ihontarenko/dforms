1e475683c19eb1eaf699bd88779fe3a1b3c7dbd6
Ivan Hontarenko <ivan_hontarenko@epam.com>
2023-02-10 10:43:42 +0200

    reconf

--- api/src/main/java/io/startform/api/StartFormApiApplication.java
+++ api/src/main/java/io/startform/api/StartFormApiApplication.java
@@ -1,17 +1,16 @@
 package io.startform.api;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.stream.Stream;
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "io.startform.parent.jpa")
 public class StartFormApiApplication {
 
     public static void main(String... arguments) {
-        SpringApplication.run(StartFormApiApplication.class, arguments);
+        run(StartFormApiApplication.class, arguments);
     }
 
 }
--- web/src/main/java/io/startform/web/StartFormWebApplication.java
+++ web/src/main/java/io/startform/web/StartFormWebApplication.java
@@ -1,17 +1,16 @@
 package io.startform.web;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class StartFormWebApplication {
 
     public static void main(String... arguments) {
-        SpringApplication.run(StartFormWebApplication.class, arguments);
+        run(StartFormWebApplication.class, arguments);
     }
 
 }
