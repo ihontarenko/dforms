37993662cf9af2c88333c7ecc2e4bf43d2e60a6b
Ivan_Hontarenko <Ivan.Hontarenko@kingfisher.com>
2024-09-20 12:06:54 +0300

    200924-M2 [context refactoring]

--- core/src/main/java/df/base/common/validation/custom/ValidationContext.java
+++ core/src/main/java/df/base/common/validation/custom/ValidationContext.java
@@ -1,32 +1,18 @@
 package df.base.common.validation.custom;
 
+import df.base.common.context.AbstractAttributesContext;
+import df.base.common.context.AttributesContext;
 import org.springframework.validation.BindingResult;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public interface ValidationContext {
+public interface ValidationContext extends AttributesContext {
 
     String BINDING_RESULT_KEY     = BindingResult.MODEL_KEY_PREFIX + ValidationContext.class.getSimpleName();
     String VALIDATION_MANAGER_KEY = "VALIDATION_MANAGER_KEY";
 
-    <T> T getAttribute(Object key);
-
-    void setAttribute(Object key, Object value);
-
-    class Simple implements ValidationContext {
-
-        private final Map<Object, Object> attributes = new HashMap<>();
-
-        @Override
-        public <T> T getAttribute(Object key) {
-            return (T) attributes.get(key);
-        }
-
-        @Override
-        public void setAttribute(Object key, Object value) {
-            attributes.put(key, value);
-        }
+    class Simple extends AbstractAttributesContext implements ValidationContext {
 
     }
 
