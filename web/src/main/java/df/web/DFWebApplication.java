363c387e3c8821fa2842e9b5bf702ebf87d8ca88
Ivan_Hontarenko <ihontarenko@gmail.com>
2025-01-23 21:32:50 +0200

    io, env development

--- web/src/main/java/df/web/DFWebApplication.java
+++ web/src/main/java/df/web/DFWebApplication.java
@@ -2,8 +2,11 @@ package df.web;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import svit.io.ClasspathResourceLoader;
+import svit.io.Resource;
+
+import java.util.Collection;
 
 import static org.springframework.boot.SpringApplication.run;
 
@@ -12,8 +15,11 @@ import static org.springframework.boot.SpringApplication.run;
 public class DFWebApplication {
 
     public static void main(String... arguments) {
-        ApplicationContext applicationContext = run(DFWebApplication.class, arguments);
-        System.out.println(applicationContext);
+        Collection<Resource> cpr = new ClasspathResourceLoader().loadResources("classpath:.");
+
+        System.out.println(cpr.size());
+//        ApplicationContext applicationContext = run(DFWebApplication.class, arguments);
+//        System.out.println(applicationContext);
     }
 
 }
