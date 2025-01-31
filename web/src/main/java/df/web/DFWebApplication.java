1cd4f0c253210d36e2e88a8522122a8bdfb7fa9b
Ivan_Hontarenko <ihontarenko@gmail.com>
2025-01-31 17:10:16 +0200

    jMouse create binding lib

--- web/src/main/java/df/web/DFWebApplication.java
+++ web/src/main/java/df/web/DFWebApplication.java
@@ -2,6 +2,7 @@ package df.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ResolvableType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import svit.ast.recognizer.EnumTokenRecognizer;
 import org.jmouse.core.io.CompositeResourceLoader;
@@ -9,6 +10,9 @@ import org.jmouse.core.io.Resource;
 import org.jmouse.core.reflection.JavaType;
 
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @EnableJpaRepositories
 @SpringBootApplication
@@ -16,17 +20,23 @@ public class DFWebApplication {
 
     public static void main(String... arguments) {
 //        Collection<Resource> cpr = new CompositeResourceLoader().findResources("jrt:java.base/**/*Calendar.class");
-        Collection<Resource> cpr = new CompositeResourceLoader().findResources("local:**/*Loader.class");
+//        Collection<Resource> cpr = new CompositeResourceLoader().findResources("local:**/*Loader.class");
+//
+//        for (Resource resource : cpr) {
+//            System.out.println(resource);
+//        }
+//
+//        System.out.println(cpr.size());
+//
+//
+//
+//         SpringApplication.run(DFWebApplication.class, arguments);
 
-        for (Resource resource : cpr) {
-            System.out.println(resource);
-        }
+        Map<Object, Object> beans = new HashMap<>();
 
-        System.out.println(cpr.size());
+        beans.put("list", List.of(1, 2, 3));
 
-
-
-         SpringApplication.run(DFWebApplication.class, arguments);
+        ResolvableType resolvableType = ResolvableType.forInstance(beans.get("list"));
     }
 
     public static void test(EnumTokenRecognizer<?> test) {
