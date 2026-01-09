d3368166e586a0f7a53d07e2d144a57f3471d678
Ivan_Hontarenko <ihontarenko@gmail.com>
2026-01-09 11:22:20 +0200

    Sync

--- common/src/main/java/df/common/persistence/PersistenceEvent.java
+++ common/src/main/java/df/common/persistence/PersistenceEvent.java
@@ -1,11 +1,13 @@
 package df.common.persistence;
 
 import org.jmouse.core.events.AbstractEvent;
+import org.jmouse.core.events.EventCategory;
+import org.jmouse.core.events.EventName;
 
 public class PersistenceEvent extends AbstractEvent<Object> {
 
     public PersistenceEvent(String name, Object payload, Object caller) {
-        super(name, payload, caller);
+        super(EventName.of(name, EventCategory.UNCATEGORIZED), payload, caller);
     }
 
 }
